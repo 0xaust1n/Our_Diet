5749ac9ea8742547e011af6b0500ea46317d9d54
AustinKuo <yo861015@gmail.com>
2019-08-22 11:42:35 +0800

    Left Navigation Alpha Test Version
    Add Left Navigation on Home Act
    Debug All Pass
    Crash on Clicking Send Buttom
    ----------------------
    Test on SDK26
    Running Fail

--- app/src/main/java/com/odstudio/ourdiet/Home.kt
+++ app/src/main/java/com/odstudio/ourdiet/Home.kt
@@ -2,34 +2,43 @@ package com.odstudio.ourdiet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import android.widget.Toolbar
+import androidx.appcompat.app.ActionBar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationView
 
 class Home : AppCompatActivity() {
     //.....分割線開始 Left Navigation
-
+    private lateinit var mDrawerLayout: DrawerLayout
 
     //.....分割線開始 Bottom Navigation Click Listener
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                val homeFragment = HomeFragment.newInstance()
-                openFragment(homeFragment)
-                return@OnNavigationItemSelectedListener true
+    private val mOnNavigationItemSelectedListener =
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> {
+                    val homeFragment = HomeFragment.newInstance()
+                    openFragment(homeFragment)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_groups -> {
+                    val groupsFragment = GroupsFragment.newInstance()
+                    openFragment(groupsFragment)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_friends -> {
+                    val friendsFragment = FriendsFragment.newInstance()
+                    openFragment(friendsFragment)
+                    return@OnNavigationItemSelectedListener true
+                }
             }
-            R.id.navigation_groups -> {
-                val groupsFragment = GroupsFragment.newInstance()
-                openFragment(groupsFragment)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_friends -> {
-                val friendsFragment = FriendsFragment.newInstance()
-                openFragment(friendsFragment)
-                return@OnNavigationItemSelectedListener true
-            }
+            false
         }
-        false
-    }
+
     private fun openFragment(fragment: Fragment) {
         val transaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.container, fragment)
@@ -46,7 +55,54 @@ class Home : AppCompatActivity() {
         val homeFragment = HomeFragment.newInstance()
         openFragment(homeFragment)
         // Declare BottomNavigation on Create event
-        val  bottomNavi: BottomNavigationView  = findViewById(R.id.navigationView)
+        val bottomNavi: BottomNavigationView = findViewById(R.id.navigationView)
         bottomNavi.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        //------------------------------------------------------------------------------------
+        // Starting Below For Drawer Navigation
+        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.drawer_layout)
+        setSupportActionBar(toolbar)
+        val actionbar: ActionBar? = supportActionBar
+        actionbar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.baseline_menu_white_24dp)
+        }
+        mDrawerLayout = findViewById(R.id.drawer_layout)
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            // set item as selected to persist highlight
+            menuItem.isChecked = true
+            // close drawer when item is tapped
+            mDrawerLayout.closeDrawers()
+
+            // Handle navigation view item clicks here.
+            when (menuItem.itemId) {
+
+                R.id.daw_personal -> {
+                    Toast.makeText(this, "@string/personal", Toast.LENGTH_LONG).show()
+                }
+                R.id.daw_setting -> {
+                    Toast.makeText(this, "@string/setting", Toast.LENGTH_LONG).show()
+                }
+                R.id.daw_logout -> {
+                    Toast.makeText(this, "@string/logout", Toast.LENGTH_LONG).show()
+                }
+            }
+            // Add code here to update the UI based on the item selected
+            // For example, swap UI fragments here
+
+            true
+        }
+
+    }
+    //appbar - toolbar button click
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                mDrawerLayout.openDrawer(GravityCompat.START)
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
--- app/src/main/java/com/odstudio/ourdiet/HomeFragment.kt
+++ app/src/main/java/com/odstudio/ourdiet/HomeFragment.kt
@@ -4,15 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toolbar
 import androidx.fragment.app.Fragment
 
 
 class HomeFragment: Fragment(){
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? = inflater.inflate(R.layout.fragment_home, container, false)
+
     companion object{
         fun  newInstance(): HomeFragment = HomeFragment()
     }
