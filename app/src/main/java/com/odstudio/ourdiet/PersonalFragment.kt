bb32473db338bf9adc241fe487b24f020cec6901
Austin <yo861015@gmail.com>
2019-09-25 15:36:06 +0800

    Fragment Test  Susccess
    Fragment Test  Susccess
    And Try GIthub Push Function

--- app/src/main/java/com/odstudio/ourdiet/PersonalFragment.kt
+++ app/src/main/java/com/odstudio/ourdiet/PersonalFragment.kt
@@ -4,15 +4,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+import org.w3c.dom.Text
 
 
-
-class PersonalFragment: Fragment(){
-    companion object{
-        fun  newInstance(): PersonalFragment = PersonalFragment()
+class PersonalFragment: Fragment() {
+    companion object {
+        fun newInstance(): PersonalFragment = PersonalFragment()
     }
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    var center: TextView? = null
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return inflater.inflate(R.layout.fragment_personal, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+     center = getView()?.findViewById(R.id.text_center)!!
+     center?.text =  FirebaseAuth.getInstance().currentUser!!.uid
     }
 }
